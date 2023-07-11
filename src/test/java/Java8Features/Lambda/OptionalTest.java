6724042447a498b4635b4affc673ba415059b3ae
kamaljit Singh <singhkamaljit64@gmail.com>
2023-07-11 16:09:59 +0530

    latest changes 2023

--- src/test/java/Java8Features/Lambda/OptionalTest.java
+++ src/test/java/Java8Features/Lambda/OptionalTest.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 public class OptionalTest {
 
 	public static void main(String[] args) {
-		Optional <String> empty = Optional.of("Wasim");
+		Optional <String> empty = Optional.of("Raul");
 		System.out.println(empty.isPresent());
 		System.out.println(empty.get());
 
